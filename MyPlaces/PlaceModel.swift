d32b9b630712dd35de296cb3991aa7de6d3c151d
Rusi0991 <ruslanismayilov484@gmail.com>
2022-07-12 15:47:45 -0700

    created PlaceModel

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Ruslan Ismayilov on 7/12/22.
+//
+
+import Foundation
+
+struct Place {
+    var name:  String
+    var location : String
+    var type : String
+    var image : String
+}
--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -11,8 +11,8 @@ class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegat
     
     @IBOutlet weak var listView: UITableView!
     
-    let restaurantNames = ["GEN", "Darda Seafood", "Kazoo", "True Food","Kabil", "Sultan Kebab", "California Wet Burrito"]
-    
+//    let restaurantNames = ["GEN", "Darda Seafood", "Kazoo", "True Food","Kabil", "Sultan Kebab", "California Wet Burrito"]
+    let places = [Place(name: "GEN", location: "Fremont", type: "Restaurant", image: "GEN")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +23,21 @@ class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegat
     // Mark Table view data source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-        cell.nameLabel.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text =  places[indexPath.row].type
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
         
         return cell
     }
     
-    
-    // Mark : -Table View Delegate
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 85
