ad150ff56e3adfc6c72c788b634a8ea61fbe7ee9
Rusi0991 <ruslanismayilov484@gmail.com>
2022-07-14 10:47:29 -0700

    Action sheet added for picking images

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -18,6 +18,21 @@ class NewPlaceViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0{
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                // chooseImagePicker
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                // chooseImagePicker
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            present(actionSheet, animated: true)
             
         } else {
             view.endEditing(true)
