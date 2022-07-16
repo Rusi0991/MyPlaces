15be27ce867b9d9d33131ec4b8d31b3591b1487e
Rusi0991 <ruslanismayilov484@gmail.com>
2022-07-15 23:30:10 -0700

    Action Sheet custimization

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,14 +19,23 @@ class NewPlaceViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0{
+            
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
+            
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
                 self.chooseimagePicker(source: .camera)
             }
+            camera.setValue(cameraIcon, forKey: "image")
+//            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlingnment")
+            
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.chooseimagePicker(source: .photoLibrary)
             }
+            photo.setValue(photoIcon, forKey: "image")
+//            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             
@@ -50,14 +60,22 @@ extension NewPlaceViewController : UITextFieldDelegate{
 
 // MARK: Work with image
 
-extension NewPlaceViewController{
+extension NewPlaceViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     func chooseimagePicker(source : UIImagePickerController.SourceType){
         if UIImagePickerController.isSourceTypeAvailable(source){
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
         }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
     }
 }
 
